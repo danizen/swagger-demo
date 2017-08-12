088631e8990239684b0eb513927a8f4554dfc3ce
Daniel Davis <dan@danizen.net>
2017-08-11 22:50:45 -0400

    Create a swagger view and beginning to add model.

--- conf/urls.py
+++ conf/urls.py
@@ -1,15 +1,20 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.http import HttpResponse
+from rest_framework_swagger.views import get_swagger_view
 
 
 def home_view(request):
     return HttpResponse(b'This app is empty')
 
 
+swagger_view = get_swagger_view(title='Pastebin API', url='/api')
+
+
 urlpatterns = [
     # Examples:
     url(r'^$', home_view, name='home'),
+    url(r'^swagger/', swagger_view, name='swagger'),
     # url(r'^blog/', include('blog.urls')),
 ]
 
--- cuesheet/models.py
+++ cuesheet/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+
+User = get_user_model()
+
+
+class Route(models.Model):
+    name = models.TextField(max_length=64, unique=True, db_index=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    public = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return "Route(id=%s, name='%s')" % (str(self.id), self.name)
+
+
+class Cue(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    cue = models.TextField(max_length=64)
+    mileage = models.IntegerField()
+
+    def __str__(self):
+        return "%s at %d" % (self.cue, self.mileage)
+
+    def __repr__(self):
+        return "Cue(id=%s, cue='%s',mileage=%d, route='%s')" % (str(self.id), self.cue, self.mileage, self.route)
 
